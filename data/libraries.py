78ecb3c695b829052e1595e373a293c0e9014108
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-29 15:50:25 +0530

    Update libraries.py

--- data/libraries.py
+++ data/libraries.py
@@ -3,3 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
+%matplotlib inline
+import matplotlib
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import  ElasticNet
+from sklearn.linear_model import  Ridge
+from sklearn.model_selection import cross_val_score
+import  statsmodels.formula.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import  Lasso
+from sklearn.model_selection import GridSearchCV
