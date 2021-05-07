c8f010556ce7c9376f2c5a02b75746de0c7ff14d
elloise <49631121+KalllN@users.noreply.github.com>
2021-05-07 19:53:45 +0530

    Update pipelines.py

--- modelling/pipelines.py
+++ modelling/pipelines.py
@@ -1,25 +1,33 @@
 class pipe_f(BaseEstimator, TransformerMixin):
     def __init__(self, bedrooms = True):
         self.bedrooms = bedrooms
+    
     def fit(self, X, y = None):
         return self
+    
     def transform(self, X):
         rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
         population_house = X[:, population_ix] / X[:, households_ix]
+        
         if self.bedrooms:
             bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
             return np.c_[X, rooms_per_household, population_house,
                          bedrooms_per_room]
+        
         else:
             return np.c_[X, rooms_per_household, population_house]
 
-num_pipeline = Pipeline([('imputer', SimpleImputer(strategy="median")),
-    ('attribs_adder', pipe_f()), ('std_scaler', StandardScaler()),])
+num_pipeline = Pipeline([('imputer', 
+                          SimpleImputer(strategy = "median")),
+    ('attribs_adder', pipe_f()), 
+                         ('std_scaler', StandardScaler()),])
 
-num_attribs = list(train_data_nums) 
+num_attribs = list(train_data_nums)
+
 cat_attribs = ["ocean_proximity"]
 
-full_pipeline = ColumnTransformer([("num", num_pipeline, num_attribs),
+full_pipeline = ColumnTransformer([("num", 
+                                    num_pipeline, num_attribs),
     ("cat", OneHotEncoder(), cat_attribs),])
 
 train_prepared = full_pipeline.fit_transform(train_data)
