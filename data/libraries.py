010b1fba0279367c5b49134a45903cb8c6269ea8
elloise <49631121+KalllN@users.noreply.github.com>
2021-05-07 19:51:05 +0530

    Update libraries.py

--- data/libraries.py
+++ data/libraries.py
@@ -1,18 +1,32 @@
+#dataset
 import pandas as pd
 import numpy as np
+
+#exploratory data analysis
 import matplotlib.pyplot as plt
 import plotly.express as px
-from sklearn.model_selection import train_test_split
 %matplotlib inline
 import matplotlib
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
+
+#modelling
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import  ElasticNet
 from sklearn.linear_model import  Ridge
-from sklearn.model_selection import cross_val_score
 import  statsmodels.formula.api as sm
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import  Lasso
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score 
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
