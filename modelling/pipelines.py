119e91cd8623e20ebd2f9a0482a0ea91431a5349
elloise <49631121+KalllN@users.noreply.github.com>
2021-05-09 20:52:38 +0530

    Update pipelines.py

--- modelling/pipelines.py
+++ modelling/pipelines.py
@@ -1,22 +1,3 @@
-class pipe_f(BaseEstimator, TransformerMixin):
-    def __init__(self, bedrooms = True):
-        self.bedrooms = bedrooms
-    
-    def fit(self, X, y = None):
-        return self
-    
-    def transform(self, X):
-        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
-        population_house = X[:, population_ix] / X[:, households_ix]
-        
-        if self.bedrooms:
-            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, population_house,
-                         bedrooms_per_room]
-        
-        else:
-            return np.c_[X, rooms_per_household, population_house]
-
 num_pipeline = Pipeline([('imputer', 
                           SimpleImputer(strategy = "median")),
     ('attribs_adder', pipe_f()), 
@@ -31,4 +12,5 @@ full_pipeline = ColumnTransformer([("num",
     ("cat", OneHotEncoder(), cat_attribs),])
 
 train_prepared = full_pipeline.fit_transform(train_data)
+
 test_prepared = full_pipeline.fit_transform(test_data)
