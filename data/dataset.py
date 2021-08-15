43b14fd445e87894051e93cb45c8bbbe07878295
Kap <49631121+KalllN@users.noreply.github.com>
2021-08-15 11:11:10 +0530

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,27 @@
+#dataset
+import pandas as pd
+import numpy as np
+
+#exploratory data analysis
+import matplotlib.pyplot as plt
+import plotly.express as px
+%matplotlib inline
+import matplotlib
+
+#modelling
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 !unzip california_housing.zip
 
 housing = pd.read_csv('housing.csv')
